6ed27247e3baf51bd088ff4f18265f5dc00e4930
MaximeRig <maxime.rigaud.dev@gmail.com>
2022-01-05 17:41:11 +0100

    manage empty url in research setting

--- src/index.php
+++ src/index.php
@@ -9,30 +9,37 @@ use App\Handler\HtmlHandler;
 use App\Error\ClientError;
 
 
-foreach($research as $siteName => $search) {
+foreach($research as $siteName => $products) {
+
+    foreach($products as $product => $search) {
+
+        try {
+
+            if (empty($search['url'])) {
+                $errorMsg = 'url is empty (' . $page->getSiteName() . ' / ' . $product . ')';
+                throw new \ErrorException($errorMsg, 0, E_ERROR, "app_error.log");
+            }
+            
+            $page = new Page($siteName, $search['url'], $search['xpath'], $search['stringToSearch']);
+            $html = Http::request($page->getUrl());
+            $isAvailable = false;
     
-    try {
-        
-        $page = new Page($siteName, $search['url'], $search['xpath'], $search['stringToSearch']);
-        $html = Http::request($page->getUrl());
-        $isAvalaible = false;
-
-        if (empty($html)) {
-            $errorMsg = 'fail to load url content (' . $page->getSiteName() . ')';
-            throw new \ErrorException($errorMsg, 0, E_ERROR, "app_error.log");
+            if (empty($html)) {
+                $errorMsg = 'fail to load url content (' . $page->getSiteName() . ')';
+                throw new \ErrorException($errorMsg, 0, E_ERROR, "app_error.log");
+            }
+            
+            $isAvailable = HtmlHandler::run($page, $html);
+    
+            if ($isAvailable) {
+                // send an email to inform that the console is available
+            }
+    
+            var_dump($isAvailable);
+    
+        } catch(ErrorException $error) {
+            $clientError = new ClientError($error);
+            $clientError->write();
         }
-        
-        $isAvalaible = HtmlHandler::run($page, $html);
-
-        if ($isAvalaible) {
-            // send an email to inform that the console is available
-        }
-
-        var_dump($isAvalaible);
-
-    } catch(ErrorException $error) {
-        $clientError = new ClientError($error);
-        $clientError->write();
     }
-
 }
--- src/research.php
+++ src/research.php
@@ -2,13 +2,27 @@
 
 $research = [
     'boulanger' => [
-        'url' => 'https://www.boulanger.com/ref/1147567',
-        'xpath' => '//*[@id="product_1649641"]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div/div[3]',
-        'stringToSearch' => 'Retrait magasin ou drive indisponible',
+        'playstation5Standard' => [
+            'url' => 'https://www.boulanger.com/ref/1147567',
+            'xpath' => '//*[@id="product_1649641"]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div/div[3]',
+            'stringToSearch' => 'Retrait magasin ou drive indisponible',
+        ],
+        'xboxSeriesX' => [
+            'url' => '',
+            'xpath' => '',
+            'stringToSearch' => '',
+        ],
     ],
     'amazonFr' => [
-        'url' => 'https://www.amazon.fr/Playstation-Sony-PlayStation-5/dp/B08H93ZRK9',
-        'xpath' => '//*[@id="availability"]/span/txt()',
-        'stringToSearch' => 'Actuellement indisponible.',
+        'playstation5Standard' => [
+            'url' => 'https://www.amazon.fr/Playstation-Sony-PlayStation-5/dp/B08H93ZRK9',
+            'xpath' => '//*[@id="availability"]/span/txt()',
+            'stringToSearch' => 'Actuellement indisponible.',
+        ],
+        'xboxSeriesX' => [
+            'url' => '',
+            'xpath' => '',
+            'stringToSearch' => '',
+        ],
     ]
 ];
