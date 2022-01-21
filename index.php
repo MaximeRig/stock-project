e1cef3a0b80cb32feb5cb59daf1a13d5d16183e0
MaximeRig <maxime.rigaud.dev@gmail.com>
2022-01-21 16:36:47 +0100

    clean index.php

--- index.php
+++ index.php
@@ -3,41 +3,9 @@
 require './vendor/autoload.php';
 include_once "./src/Data/researches.php";
 
-// use App\Model\Product;
-// use App\Client\Http;
-// use App\Handler\HtmlHandler;
-// use App\Error\ClientError;
 use App\Utils\DotEnv;
 
 (new DotEnv(__DIR__ . '/.env.local'))->load();
 
-// lancer une instance de App (faire un singleton)
-// créer une méthode dans App pour lancer le foreach du dessous
-
 $app = App\App::getInstance();
-var_dump($app->run());
-
-/*
-foreach($researches as $research) {
-
-    try {
-
-        $product = new Product($research);
-        
-        $html = Http::request($product);
-        $isAvailable = false;
-        
-        $isAvailable = HtmlHandler::run($product, $html);
-
-        if ($isAvailable) {
-            // send an email to inform that the console is available
-        }
-
-        var_dump($isAvailable);
-
-    } catch(\Exception $error) {
-        $clientError = new ClientError($error);
-        $clientError->write();
-    }
-}
-*/
+$app->run();
--- src/App.php
+++ src/App.php
@@ -15,4 +15,30 @@ class App {
         return self::$instance;
     }
 
+    public function run() {
+        /*
+        foreach($researches as $research) {
+
+            try {
+
+                $product = new Product($research);
+                
+                $html = Http::request($product);
+                $isAvailable = false;
+                
+                $isAvailable = HtmlHandler::run($product, $html);
+
+                if ($isAvailable) {
+                    // send an email to inform that the console is available
+                }
+
+                var_dump($isAvailable);
+
+            } catch(\Exception $error) {
+                $clientError = new ClientError($error);
+                $clientError->write();
+            }
+        }
+        */
+    }
 }
